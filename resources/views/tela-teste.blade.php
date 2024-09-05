83fb0cc76786d99a8c0405dedd4ea56beaf3746e
RaldoLukas <raldolukas211@gmail.com>
2024-09-05 10:39:02 -0300

    criacao do projeto, tailwind, e tabelas

---
+++ resources/views/tela-teste.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Faz o L</title>
+</head>
+<body>
+    
+<h1 class="bg-black text-white p-4 rounded-xl ">Pai Aqui é Foda</h1>
+
+
+
+</body>
+</html>
